ade6a09aa089aa982c6d34657fabab3a89694e82
kartinich <dkostrikin@inbox.ru>
2022-07-11 21:11:20 +0300

    devise

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ApplicationRecord
+  belongs_to :user, optional: true
+
   validates :title, presence: true, length: {minimum: 2}
   validates :body, presence: true, length: {minimum: 2}
 
   def formatted_created_at
-    created_at.strftime('%Y-%M-%D %H:%M:%S')
+    created_at.strftime('%d-%m-%Y %H:%M:%S')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_08_090434) do
+ActiveRecord::Schema.define(version: 2022_07_11_170547) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "posts", "users"
 end
